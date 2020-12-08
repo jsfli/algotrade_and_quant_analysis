7c2fe21c14e058ad46314d3aa55b47dbebed0f2d
Jeffrey Li <jeffrey.li@cantab.net>
2020-12-08 17:24:33 +0800

    added technical indicators and yfinance

--- technical_indicators.py
+++ technical_indicators.py
@@ -7,12 +7,13 @@ key="RZ9052SXUN5R3GJO"
 
 ts = TimeSeries(key=key, output_format="pandas")
 
-msft, meta_data = ts.get_daily_adjusted(symbol="MSFT", outputsize="full")
+msft, meta_data = ts.get_daily_adjusted(symbol="MSFT", outputsize="compact")
 
 
 def MACD(DF, fast, slow, signal):
     df = DF.copy()
     df.columns = ["open","high","low","close","adjusted_close","volume","dividend","split"]
+    df = df[::-1]
     df["MACD_Fast"] = df["adjusted_close"].ewm(span=fast, min_periods=fast).mean()
     df["MACD_Slow"] = df["adjusted_close"].ewm(span=slow, min_periods=slow).mean()
     df["MACD"] = df["MACD_Fast"] - df["MACD_Slow"]
@@ -27,10 +28,16 @@ def ATR(DF, n):
     """
     df = DF.copy()
     df.columns = ["open", "high", "low", "close", "adjusted_close", "volume", "dividend", "split"]
-    df["H-L"] = abs(df["high"] - df["low"])
-    df["H-PC"] = abs(df["high"] - df["adjusted_close"].shift(1))
-    df["L-PC"] = abs(df["low"] - df["adjusted_close"].shift(1))
-    df["TR"] = df[["H-L"],["H-PC"],["L-PC"]].max(axis=1, skipna=False)
-    df["ATR"] = df["TR"].rolling(n).mean()
-    df2 = df.drop(["H-L","H-PC", "L-PC"], axis=1)
+    df = df[::-1]
+    df['H-L'] = abs(df["high"] - df["low"])
+    df['H-PC'] = abs(df["high"] - df["adjusted_close"].shift(1))
+    df['L-PC'] = abs(df["low"] - df["adjusted_close"].shift(1))
+    df['TR'] = df[["H-L", "H-PC", "L-PC"]].max(axis=1, skipna=False)
+    df['ATR'] = df["TR"].rolling(n).mean()
+    df2 = df.drop(['H-L','H-PC', 'L-PC'], axis=1)
     return df2
+
+
+data = ATR(msft, 20)
+
+data[["adjusted_close", "volume","ATR"]].plot(subplots=True, layout=(3,1))
